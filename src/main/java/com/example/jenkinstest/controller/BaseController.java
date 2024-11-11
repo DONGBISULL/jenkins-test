4a73c68686efd118dfb539ef5b42bf1ea7471518
DONGBISULL <sdb7734@gmail.com>
2024-11-11 22:54:33 +0900

    인덱스페이지 수정

--- src/main/java/com/example/jenkinstest/controller/BaseController.java
+++ src/main/java/com/example/jenkinstest/controller/BaseController.java
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.ServletContext;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,7 +18,11 @@ public class BaseController {
     @GetMapping("/")
     protected String index(Model model) {
         String serverInfo = servletContext.getServerInfo();
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDate = now.format(formatter);
         model.addAttribute("serverInfo", serverInfo);
+        model.addAttribute("today", formattedDate);
         return "index";
     }
 
